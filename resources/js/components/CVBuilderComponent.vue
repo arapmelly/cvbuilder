ee76e96e0efdd30ae6edfce5c14de177bf177445
SCANGROUP\walhaddad <Wasim.Al-Haddad@squaddigital.com>
2021-08-07 13:13:16 +0300

    UI changes

--- resources/js/components/CVBuilderComponent.vue
+++ resources/js/components/CVBuilderComponent.vue
@@ -1,38 +1,34 @@
 <template>
+
     <div>
-
-    <div class="row">
-
-                
-                <div class="col-4">
-                    <editor-component :resume="resume"></editor-component>
-                </div>
-
-                <div class="col-8 bg-white" id="pdf">
-
-                    <preview-component :resume="resume"></preview-component>
-                </div>
-
+        <div class="row">
+            <div class="col-4">
+                <editor-component :resume="resume"></editor-component>
             </div>
 
+            <div class="col-8 bg-white" id="pdf">
 
+                <preview-component :resume="resume"></preview-component>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: "CVBuilderComponent",
-  props: {
-      resume_data: {
-        type: Object,
-        required: true
-      }
+    name: "CVBuilderComponent",
+    props: {
+        resume_data: {
+            type: Object,
+            required: true
+        }
     },
-    data () {
-    return {
-      resume: this.resume_data
-    }
-  },
+    data() {
+        return {
+            resume: this.resume_data
+        }
+    },
 }
 </script>
 
